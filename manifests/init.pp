38944adda3ebce3e78d8c4e4eeacf949bd19789a
Zack Smith <acidprime@wallcity.org>
2013-06-12 14:08:11 -0700

    Initial Commit

---
+++ manifests/init.pp
@@ -0,0 +1,62 @@
+# == Class: openswan
+#
+# Full description of class openswan here.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
+#
+# === Variables
+#
+# Here you should define a list of variables that this module would require.
+#
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if it
+#   has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should not be used in preference to class parameters  as of
+#   Puppet 2.6.)
+#
+# === Examples
+#
+#  class { openswan:
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
+#  }
+#
+# === Authors
+#
+# Author Name <author@domain.com>
+#
+# === Copyright
+#
+# Copyright 2011 Your name here, unless otherwise noted.
+#
+class openswan {
+  service { 'ipsec':
+    ensure  => running,
+    enable  => true,
+    require => Package['openswan'],
+  }
+
+  package { 'openswan':
+    ensure => '2.6.32-20.el6_4',
+  }
+  file { '/etc/ipsec.secrets':
+    ensure  => file,
+    content => template("${module_name}/ipsec.secrets.erb"),
+  }
+  file { '/etc/ipsec.conf':
+    ensure  => file,
+    content => template("${module_name}/ipsec.conf.erb"),
+    notify  => Service['ipsec'],
+  }
+
+  Service <| title == 'racoon' |> {
+    ensure => stopped,
+    enable => false,
+  }
+}
